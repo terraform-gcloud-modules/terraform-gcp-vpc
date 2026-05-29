e27970490d8ec033ed0d8ed780876087d074fc58
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2026-05-29 22:05:24 +0530

    fix: updated workflows

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,14 +1,15 @@
 ##-----------------------------------------------------------------------------
 ## Versions
 ##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.14, < 2.0"
+}
 
+terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0.0, < 6.0.0"
+      version = ">= 4.64, < 8"
     }
   }
 }
